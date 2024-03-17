25fcbbcdb47f8d431badcc96569d1e0c112d684a
Piper <77084364+pipemm@users.noreply.github.com>
2024-03-17 15:26:38 +0000

    fixing

--- Azure/Lab/automate-azure-tasks-with-powershell/02_Create_Linux_VMs.ps1
+++ Azure/Lab/automate-azure-tasks-with-powershell/02_Create_Linux_VMs.ps1
@@ -2,21 +2,24 @@
 ## https://learn.microsoft.com/en-us/training/modules/automate-azure-tasks-with-powershell/6-exercise-create-resource-interactively
 
 [System.Int32] $NumberVM      = [System.Int32]${Env:NUM_VMS};
-if ( $(NumberVM} -eq $null ) {
+if ( ${NumberVM} -eq $null ) {
     [System.Int32] $NumberVM  = [System.Int32]1;
-} elseif ( $(NumberVM} -lt 1) {
+} elseif ( ${NumberVM} -lt 1) {
     [System.Int32] $NumberVM  = [System.Int32]1;
-} elseif ( $(NumberVM} -gt 9) {
+} elseif ( ${NumberVM} -gt 9) {
     [System.Int32] $NumberVM  = [System.Int32]9;
 }
 [System.Int32] $port_ssh_open  = 22;
 [System.String]$ImageVM        = "${Env:IMMAGE}";
-[System.String]$ResourceGroup  = "${Env:RESOURCEGROUPNAME}";
-[System.String]$LocationVM     = "${Env:LOCATION}";
+
+
 [System.String]                $username    =  "${Env:VM_USERNAME}";
 [System.Security.SecureString] $password    = ("${Env:VM_PASSWORD}" | ConvertTo-SecureString -AsPlainText -Force);
 [System.Management.Automation.PSCredential] $Credential = [System.Management.Automation.PSCredential]::New(${username},${password});
-[System.String]                $UsernameVM = ${Credential}.Username;
+[System.String]                $UsernameVM  = ${Credential}.Username;
+
+[System.String]$ResourceGroup  = "${Env:RESOURCEGROUPNAME}";
+[System.String]$LocationVM     = "${Env:LOCATION}";
 
 if ( "${ResourceGroup}" -eq '' ) {
     Write-Error -Message 'There is no resource group found. '

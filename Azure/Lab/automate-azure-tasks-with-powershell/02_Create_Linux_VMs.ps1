39efc550ee5392dbdb8cb0ad85dc341080db3f6d
Piper <77084364+pipemm@users.noreply.github.com>
2024-03-17 12:28:30 +0000

    update port inputs

--- Azure/Lab/automate-azure-tasks-with-powershell/02_Create_Linux_VMs.ps1
+++ Azure/Lab/automate-azure-tasks-with-powershell/02_Create_Linux_VMs.ps1
@@ -1,49 +1,50 @@
 
 ## https://learn.microsoft.com/en-us/training/modules/automate-azure-tasks-with-powershell/6-exercise-create-resource-interactively
 
-[System.Int32] $number_vm      = [System.Int32]${Env:NUM_VMS};
-if ( $(number_vm} -eq $null ) {
-    [System.Int32] $number_vm  = [System.Int32]1;
-} elseif ( $(number_vm} -lt 1) {
-    [System.Int32] $number_vm  = [System.Int32]1;
-} elseif ( $(number_vm} -gt 9) {
-    [System.Int32] $number_vm  = [System.Int32]9;
+[System.Int32] $NumberVM      = [System.Int32]${Env:NUM_VMS};
+if ( $(NumberVM} -eq $null ) {
+    [System.Int32] $NumberVM  = [System.Int32]1;
+} elseif ( $(NumberVM} -lt 1) {
+    [System.Int32] $NumberVM  = [System.Int32]1;
+} elseif ( $(NumberVM} -gt 9) {
+    [System.Int32] $NumberVM  = [System.Int32]9;
 }
 [System.Int32] $port_ssh_open  = 22;
-[System.String]$image_vm       = 'Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest';
-[System.String]$resource_group = "${Env:RESOURCEGROUPNAME}";
-[System.String]$location_vm    = "${Env:LOCATION}";
+[System.String]$ImageVM        = "${Env:IMMAGE}";
+[System.String]$ResourceGroup  = "${Env:RESOURCEGROUPNAME}";
+[System.String]$LocationVM     = "${Env:LOCATION}";
 [System.String]                $username    =  "${Env:VM_USERNAME}";
 [System.Security.SecureString] $password    = ("${Env:VM_PASSWORD}" | ConvertTo-SecureString -AsPlainText -Force);
-[System.Management.Automation.PSCredential] $credential = [System.Management.Automation.PSCredential]::New(${username},${password});
-[System.String]                $username_vm = ${credential}.Username;
+[System.Management.Automation.PSCredential] $Credential = [System.Management.Automation.PSCredential]::New(${username},${password});
+[System.String]                $UsernameVM = ${Credential}.Username;
 
-if ( "${resource_group}" -eq '' ) {
+if ( "${ResourceGroup}" -eq '' ) {
     Write-Error -Message 'There is no resource group found. '
     exit 1;
 }
 
-Write-Output "Resource Group               : ${resource_group}";
-Write-Output "Virtual Machine Location     : ${location_vm}";
-Write-Output "Virtual Machine Image        : ${image_vm}";
+Write-Output "Resource Group               : ${ResourceGroup}";
+Write-Output "Virtual Machine Location     : ${LocationVM}";
+Write-Output "Virtual Machine Image        : ${ImageVM}";
 Write-Output "Port for Secure Shell (Open) : ${port_ssh_open}";
-Write-Output "Virtual Machine User Name    : ${username_vm}";
-Write-Output "Number of Machines to be disployed : ${number_vm}";
+Write-Output "Virtual Machine User Name    : ${UsernameVM}";
+Write-Output "Number of Machines to be disployed : ${NumberVM}";
 
-[System.String]  $prefix_vm  = 'testvm';
-[System.Int32[]] $ports_open = @(${port_ssh_open});
+[System.String]  $PrefixVM  = 'testvm';
+[System.Int32[]] $PortsOpen = @(${port_ssh_open});
 
-foreach( $n in 1..${number_vm} ){
-    [System.String]$name_vm = "${prefix_vm}-${location_vm}-" + ${n}.ToString().PadLeft(3,'0')
-    Write-Output "Deploying Virtual Machine : ${name_vm}";
+foreach( $n in 1..${NumberVM} ){
+    [System.String]$NameVM = "${PrefixVM}-${LocationVM}-" + ${n}.ToString().PadLeft(3,'0')
+    Write-Output "Deploying Virtual Machine : ${NameVM}";
     [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$vm = 
         (
-            New-AzVm -ResourceGroupName "${resource_group}" -Name "${name_vm}" `
-                -Credential ${credential} `
-                -Location "${location_vm}" `
-                -Image "${image_vm}" `
-                -OpenPorts ${ports_open} `
-                -PublicIpAddressName "${name_vm}"
+            New-AzVm -ResourceGroupName "${ResourceGroup}"`
+                -Name       "${NameVM}" `
+                -Credential  ${Credential} `
+                -Location   "${LocationVM}" `
+                -Image      "${ImageVM}" `
+                -OpenPorts   ${PortsOpen} `
+                -PublicIpAddressName "${NameVM}"
         );
     Write-Output ${vm};
 }

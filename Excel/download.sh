ade8b0d800beafdd99861138813866bc1aec32da
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-29 16:20:43 +0000

    update

--- Excel/download.sh
+++ Excel/download.sh
@@ -2,13 +2,8 @@
 
 url_index='https://create.microsoft.com/en-us/search?filters=excel'
 
-curl "${url_index}"
+##curl "${url_index}"
 
 
-curl 'https://create.microsoft.com/api/graphql' \
-  -H 'accept: */*' \
-  -H 'content-type: application/json' \
-  -H 'origin: https://create.microsoft.com' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' \
-  --data '@request.ql' |
+python3 get_template_list.py |
   python -m json.tool > test.json
--- Excel/get_template_list.py
+++ Excel/get_template_list.py
@@ -2,54 +2,35 @@ import requests
 import json
 from collections import OrderedDict
 
-url_api = 'https://create.microsoft.com/api/graphql'
+url_api    = 'https://create.microsoft.com/api/graphql'
+url_origin = 'https://create.microsoft.com'
 
-with open("request.ql", "r") as f:
-    request_body = f.read()
-
-def req( offset=0, limit=50 ):
+def request_data( offset=0, limit=50 ):
     from json import dumps
     request_dict   = OrderedDict()
     variables_dict = OrderedDict()
+    variables_dict['query']   = '*'
+    variables_dict['filters'] = ['content-type=excel']
+    variables_dict['offset']  = offset
+    variables_dict['limit']   = limit
+    variables_dict['generic'] = False
     request_dict['operationName'] = 'getSearchTemplateGrid'
-    variables_dict['query']       = '*'
-    variables_dict['filters']     = ['content-type=excel']
-    variables_dict['offset']      = 0
-    variables_dict['limit']       = 50
-    variables_dict['generic']     = False
     request_dict['variables']     = variables_dict
-    request_dict['query']         = "query getSearchTemplateGrid($query: String!, $filters: [String!], $offset: Int, $limit: Int, $locale: String, $generic: Boolean, $collectionId: String, $orderSeed: String) {\n searchTemplates(\n query: $query\n filters: $filters\n locale: $locale\n offset: $offset\n limit: $limit\n generic: $generic\n collectionId: $collectionId\n orderSeed: $orderSeed\n ) {\n id\n ...SearchTemplateGrid_searchTemplates\n __typename\n }\n}\n\nfragment SearchTemplateGrid_searchTemplates on SearchTemplates {\n templates {\n templates {\n id\n ...TemplateThumbnailCard_template\n __typename\n }\n __typename\n }\n totalCount\n searchStatus\n __typename\n}\n\nfragment TemplateThumbnailCard_template on Template {\n title\n longFormTitle\n premium\n templateContentType\n __typename\n}"
+    with open("query.graphql", "r") as f:
+        request_dict['query'] = f.read()
     return dumps(request_dict)
 
-request_body = r"""
-    {
-        "operationName": "getSearchTemplateGrid",
-        "variables": {
-            "query": "*",
-            "filters": [
-                "content-type=excel"
-            ],
-            "offset": 0,
-            "limit": 50,
-            "generic": false
-        },
-        "query": "query getSearchTemplateGrid($query: String!, $filters: [String!], $offset: Int, $limit: Int, $locale: String, $generic: Boolean, $collectionId: String, $orderSeed: String) {\n searchTemplates(\n query: $query\n filters: $filters\n locale: $locale\n offset: $offset\n limit: $limit\n generic: $generic\n collectionId: $collectionId\n orderSeed: $orderSeed\n ) {\n id\n ...SearchTemplateGrid_searchTemplates\n __typename\n }\n}\n\nfragment SearchTemplateGrid_searchTemplates on SearchTemplates {\n templates {\n templates {\n id\n ...TemplateThumbnailCard_template\n __typename\n }\n __typename\n }\n totalCount\n searchStatus\n __typename\n}\n\nfragment TemplateThumbnailCard_template on Template {\n title\n longFormTitle\n premium\n templateContentType\n __typename\n}"
-    }
-"""
+request_body = request_data( )
 
 headers = {
     "Accept": "*/*",
     "Content-Type": "application/json",
-    "Origin": "https://create.microsoft.com",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Origin": url_origin,
 }
 
-print( req() )
+from urllib.request import Request, urlopen
+req = Request(url=url_api, data=request_body.encode(), headers=headers)
+with urlopen(req) as f:
+    print(f.read().decode('utf-8'))
 
-#response = requests.post(url_api, headers=headers, data=request_body)
 
-
-#if response.status_code == 200:
-#    print("Success! Response:", response.json())
-#else:
-#    print("Error:", response.status_code, response.text)

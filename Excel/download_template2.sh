b3adc4955a7189c3ba1cd01cb5d3c3da07d697c8
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-30 18:07:08 +0000

    update

---
+++ Excel/download_template2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+thisscript=$(realpath "${0}")
+thispath="${thisscript%/*}/"
+cd "${thispath}"
+
+url_index='https://create.microsoft.com/search?filters=excel'
+url_template='https://create.microsoft.com/template/'
+
+folder_template='Template/'
+mkdir --parent "${folder_template%/}/"
+
+python3 get_template_list.py 'QL/query.graphql' |
+  while read -r line
+  do
+    url_page="${url_template%/}/${line%%   *}"
+    filename="${line##*   }.xlsx"
+    filename="${filename//[?]/ }"
+    echo "visiting ${url_page}"
+    url_file=$(
+      curl --silent --location "${url_page}" |
+      sed --silent 's!^.*"link":"\([^"]*\.xlsx\)".*$!\1!p' |
+      head --lines=1
+    )
+    echo "${url_file}   ${filename}"
+    ##curl --silent --output "${folder_template%/}/${filename}" "${url_file}"
+  done
+

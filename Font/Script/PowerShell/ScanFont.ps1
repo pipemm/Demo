a66f338691e22fe77227b2df2a1c4eb788b0dce1
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-26 15:01:45 +0000

    update

--- Font/Script/PowerShell/ScanFont.ps1
+++ Font/Script/PowerShell/ScanFont.ps1
@@ -5,6 +5,7 @@ ForEach-Object -Process {
     [System.String]$FullName = $_.FullName;
     try { 
         [System.Windows.Media.GlyphTypeface]$FontFile = New-Object -TypeName System.Windows.Media.GlyphTypeface -ArgumentList "${FullName}";
+        $FullName;
         $FontFile;
     } catch {
         Write-Warning "Ignoring ${FullName}";

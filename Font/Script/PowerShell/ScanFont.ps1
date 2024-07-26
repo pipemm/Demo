a58ad9ac1e0101e72e4aa67f990dfce7c113ed55
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-26 14:53:12 +0000

    update fonts

---
+++ Font/Script/PowerShell/ScanFont.ps1
@@ -0,0 +1,12 @@
+
+Add-Type -AssemblyName PresentationFramework;
+Get-ChildItem -LiteralPath 'C:\Windows\Fonts' |
+ForEach-Object -Process {
+    [System.String]$FullName = $_.FullName;
+    try { 
+        [System.Windows.Media.GlyphTypeface]$FontFile = New-Object -TypeName System.Windows.Media.GlyphTypeface -ArgumentList "${FullName}";
+        $FontFile;
+    } catch {
+        Write-Warning "Ignoring ${FullName}";
+    };
+}

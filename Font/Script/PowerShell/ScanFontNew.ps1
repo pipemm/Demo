201d16ab513ea74f9a814f24948b8ea80c832f08
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-28 16:51:39 +0000

    test

--- Font/Script/PowerShell/ScanFontNew.ps1
+++ Font/Script/PowerShell/ScanFontNew.ps1
@@ -6,6 +6,13 @@
 #Add-Type -AssemblyName PresentationFramework;
 Get-ChildItem -LiteralPath 'C:\Windows\Fonts' |
 ForEach-Object -Process {
-    [System.String]$FullName = $_.FullName;
-    $_ ;
+    [System.String]$FullName      = $_.FullName;
+    [System.String]$Name          = $_.Name;
+    [System.String]$PathFileBasic = (Join-Path -Path "${PathBasic}" -ChildPath "${Name}");
+    if (Test-Path "${PathFileBasic}" -PathType 'Leaf') {
+        $PathFileBasic;
+    } else {
+        $FullName ;
+    }
+    
 }

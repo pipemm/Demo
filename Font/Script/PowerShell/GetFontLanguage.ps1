5695bf8e498ed77350b162eabf6c63cd51830afe
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-29 16:24:55 +0000

    update

--- Font/Script/PowerShell/GetFontLanguage.ps1
+++ Font/Script/PowerShell/GetFontLanguage.ps1
@@ -2,13 +2,15 @@
 [System.String]$PathCJK  = 'File\CJK\';
 [System.String]$Language = "${env:Language}";
 
-Add-Type -AssemblyName PresentationFramework;
-Get-ChildItem -Recurse -File -LiteralPath "${PathCJK}" |
-    ForEach-Object -Process {
-        [System.String]$FullName = $_.FullName;
-        [System.Windows.Media.GlyphTypeface]$FontFile = New-Object -TypeName System.Windows.Media.GlyphTypeface -ArgumentList "${FullName}";
-        $FamilyNames = ${FontFile}.FamilyNames;  ## MS.Internal.Text.TextInterface.LocalizedStrings
-        $FamilyName  = (${FamilyNames} | Where-Object {$_.Key -eq "${Language}"}).Value;
-        ${FontFile} | Add-Member -MemberType NoteProperty -Name 'FamilyName' -Value ${FamilyName};
-    } | 
-    Where-Object FamilyName -NE $null
+if ( "${Language}" -ne '' ) {
+    Add-Type -AssemblyName PresentationFramework;
+    Get-ChildItem -Recurse -File -LiteralPath "${PathCJK}" |
+        ForEach-Object -Process {
+            [System.String]$FullName = $_.FullName;
+            [System.Windows.Media.GlyphTypeface]$FontFile = New-Object -TypeName System.Windows.Media.GlyphTypeface -ArgumentList "${FullName}";
+            $FamilyNames = ${FontFile}.FamilyNames;  ## MS.Internal.Text.TextInterface.LocalizedStrings
+            $FamilyName  = (${FamilyNames} | Where-Object {$_.Key -eq "${Language}"}).Value;
+            ${FontFile} | Add-Member -MemberType NoteProperty -Name 'FamilyName' -Value ${FamilyName};
+        } | 
+        Where-Object FamilyName -NE $null;
+}

b7b98507e2031a83073f2e742de583e864dee2c9
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-29 17:04:40 +0000

    update

--- Font/Script/PowerShell/GetFontLanguage.ps1
+++ Font/Script/PowerShell/GetFontLanguage.ps1
@@ -1,6 +1,8 @@
 
-[System.String]$PathCJK  = 'File\CJK\';
-[System.String]$Language = "${env:Language}";
+[System.String]$PathCJK    = 'File\CJK\';
+[System.String]$Language   = "${env:Language}";
+[System.String]$PathTarget = 'File\Target\';
+New-Item -Name "${PathTarget}" -ItemType 'directory';
 
 if ( "${Language}" -ne '' ) {
     Add-Type -AssemblyName PresentationFramework;
@@ -11,7 +13,14 @@ if ( "${Language}" -ne '' ) {
             $FamilyNames = ${FontFile}.FamilyNames;  ## MS.Internal.Text.TextInterface.LocalizedStrings
             $FamilyName  = (${FamilyNames} | Where-Object {$_.Key -eq "${Language}"}).Value;
             if ( ${FamilyName} -ne $null ) {
+                Copy-Item "${FullName}" -Destination "${PathTarget}";
+                $FaceNames = ${FontFile}.FaceNames;
+                $FaceName  = (${FaceNames} | Where-Object {$_.Key -eq "${Language}"}).Value;
+                if ( ${FaceName} -eq $null ) {
+                    $FaceName = ${FaceNames}.Values[0];
+                }
                 ${FontFile} | Add-Member -MemberType NoteProperty -Name 'FamilyName' -Value ${FamilyName};
+                ${FontFile} | Add-Member -MemberType NoteProperty -Name 'FaceName'   -Value ${FaceName};
                 ${FontFile};
             }
         } ;

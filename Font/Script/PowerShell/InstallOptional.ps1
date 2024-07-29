302f2dea252a0cf8118407e065db46f2e659a3ae
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-29 13:40:05 +0000

    update

---
+++ Font/Script/PowerShell/InstallOptional.ps1
@@ -0,0 +1,14 @@
+
+[System.String]$Language = $env:Language;
+
+if ( "${Language}" -ne '' ) {
+    Get-WindowsCapability -Online |
+        Where-Object State -EQ 'NotPresent' |
+        Where-Object Name -Match '^Language\.Fonts' |
+        Where-Object Name -Match "${Language}" |
+        ForEach-Object -Process {
+            [System.String]$Capability = $_.Name;
+            ${Capability};
+            Add-WindowsCapability -Online -Name "${Capability}";
+        }
+}

5fe41c3fa31c337d12f466dfa8ef7589d564750f
Piper <77084364+pipemm@users.noreply.github.com>
2024-07-29 16:12:08 +0000

    test

--- Font/Script/PowerShell/GetFontLanguage.ps1
+++ Font/Script/PowerShell/GetFontLanguage.ps1
@@ -2,15 +2,13 @@
 [System.String]$PathCJK  = 'File\CJK\';
 [System.String]$Language = "${env:Language}";
 
-"${Language}";
-
 Add-Type -AssemblyName PresentationFramework;
 Get-ChildItem -Recurse -File -LiteralPath "${PathCJK}" |
 ForEach-Object -Process {
     [System.String]$FullName = $_.FullName;
     $FullName;
     [System.Windows.Media.GlyphTypeface]$FontFile = New-Object -TypeName System.Windows.Media.GlyphTypeface -ArgumentList "${FullName}";
-    ${FontFile}.FamilyNames.GetType();
-    ${FontFile}.FamilyNames.GetType().FullName;
-    ${FontFile};
+    $FamilyNames = ${FontFile}.FamilyNames;
+    $FamilyName  = (${FamilyNames} | Where-Object {$_.Key -eq "${Language}"}).Value;
+    ${FontFile} | Add-Member -MemberType NoteProperty -Name 'FamilyName' -Value ${FamilyName};
 }

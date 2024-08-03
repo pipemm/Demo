c3d3e784e22bf4b9b0208792de6bdf828a593b59
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-03 10:31:42 +0000

    testing

--- MongoDB/Script/Bash/TestAPI.sh
+++ MongoDB/Script/Bash/TestAPI.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/bash
 
-URL_API="${ENDPOINT%/}/action/findOne"
-## https://www.mongodb.com/docs/atlas/app-services/data-api/openapi/#operation/aggregate
-## https://www.mongodb.com/docs/atlas/app-services/data-api/examples/
-
-data=$(
-echo '{
-    "collection": "__COLLECTION_NAME__",
-    "database":   "__DATABASE_NAME__",
-    "dataSource": "__CLUSTER__",
-    "projection": {"_id": 1}
-}' |
-  jq --arg source  "${CLUSTER}"         '.dataSource = $source'  |
-  jq --arg collect 'data'               '.collection = $collect' |
-  jq --arg db      'sample_weatherdata' '.database   = $db'
-)
-
-curl --location --request POST "${URL_API}" \
-  --header 'Content-Type: application/json' \
-  --header 'Access-Control-Request-Headers: *' \
-  --header "api-key: ${API_KEY}" \
-  --data-raw "${data}" |
-  jq
-
 URL_API="${ENDPOINT%/}/action/aggregate"
 ## https://www.mongodb.com/docs/atlas/app-services/data-api/openapi/#operation/aggregate
 ## https://www.mongodb.com/docs/atlas/app-services/data-api/examples/

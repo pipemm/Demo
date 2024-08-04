aa3d621cd89398c91f36c01d161a5dbcfc4f23f6
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-04 04:23:02 +0000

    update

--- MongoDB/Script/Bash/TestAPI.sh
+++ MongoDB/Script/Bash/TestAPI.sh
@@ -5,7 +5,7 @@ URL_API="${ENDPOINT%/}/action/aggregate"
 ## https://www.mongodb.com/docs/atlas/app-services/data-api/examples/
 
 file_data='Request/json/data-aggregate.json'
-data=$(
+datatemplate=$(
   cat "${file_data}" |
   jq --arg source  "${CLUSTER}"         '.dataSource = $source'  |
   jq --arg db      'sample_weatherdata' '.database   = $db'      |
@@ -16,7 +16,18 @@ filecatalog='Data-Catalog.json'
 cat "${filecatalog}" |
   jq '.databases | [.[] | {database:.name, collections}]' |
   jq '[.[] | .database as $database | .collections [] | {database: $database, collection: .} ]' |
-  jq --compact-output '.[]'
+  jq --compact-output '.[]' |
+  while read -r req
+  do
+    db=$(echo "${req}" | jq --raw-output '.database')
+    collection=$(echo "${req}" | jq --raw-output '.collection')
+    data=$(
+      echo "${datatemplate}" |
+      jq --arg db         "${db}"         '.database   = $db'         |
+      jq --arg collection "${collection}" '.collection = $collection'
+    )
+    echo "${data}"
+  done
 
 curl --location --request POST "${URL_API}" \
   --header 'Content-Type: application/json' \

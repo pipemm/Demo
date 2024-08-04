b15c832754f33d7b501d59282d2886275154d81e
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-04 03:47:03 +0000

    fixing

--- MongoDB/Script/Bash/TestAPI.sh
+++ MongoDB/Script/Bash/TestAPI.sh
@@ -15,7 +15,7 @@ data=$(
 filecatalog='Data-Catalog.json'
 cat "${filecatalog}" |
   jq '.databases | [.[] | {database:.name, collections}]' |
-  jq '[.[] | .database as $database | collections | [.[] | {database: $database, collection: .}] ]'
+  jq '[.[] | .database as $database | collections ]'
 
 curl --location --request POST "${URL_API}" \
   --header 'Content-Type: application/json' \

ee389afbeb884eb4862dc95218a830cd63fa239e
Piper <77084364+pipemm@users.noreply.github.com>
2025-01-08 15:59:09 +0000

    final test

---
+++ Dispatch/cleanup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+curl --location \
+  --header 'Accept: application/vnd.github+json' \
+  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+  --header 'X-GitHub-Api-Version: 2022-11-28' \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts" |
+  jq '[.artifacts[] | [.name,.id]]' |
+  jq --raw-output '.[] | @tsv' |
+  sed --silent 's/^segment-[0-9]*\t//p' |
+  while read -r artifact_id
+  do
+    curl --location \
+      --request DELETE \
+      --header 'Accept: application/vnd.github+json' \
+      --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+      --header 'X-GitHub-Api-Version: 2022-11-28' \
+      "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/artifacts/${artifact_id}"
+  done

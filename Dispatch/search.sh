be8d8e0f47ebdec931d734ca3460abb12c5f3a6c
Piper <77084364+pipemm@users.noreply.github.com>
2025-01-08 16:07:51 +0000

    okay run

--- Dispatch/search.sh
+++ Dispatch/search.sh
@@ -26,7 +26,7 @@ snumber=$(( Number + seg ))
 sname="segment-${snumber}"
 filelog="${folderlog%/}/${sname}.txt"
 
-for iid in {0..9}
+for iid in {0..999}
 do
   id=$(( snumber + iid ))
   url="${Prefix%/}/${id}"

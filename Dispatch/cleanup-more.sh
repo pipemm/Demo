4f66a52081043214e85f3e8c555752af47b691b2
Piper <77084364+pipemm@users.noreply.github.com>
2025-01-09 15:40:24 +0000

    test

--- Dispatch/cleanup-more.sh
+++ Dispatch/cleanup-more.sh
@@ -10,7 +10,7 @@ curl --location \
   --header 'Accept: application/vnd.github+json' \
   --header "Authorization: Bearer ${GITHUB_TOKEN}" \
   --header 'X-GitHub-Api-Version: 2022-11-28' \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts" |
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/artifacts" |
   jq '[.artifacts[] | [.name,.id]]' |
   jq --raw-output '.[] | @tsv' > "${filelist}"
 

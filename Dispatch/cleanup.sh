b6aa5507fe44bd045f655617b72cc4f462ed60fb
Piper <77084364+pipemm@users.noreply.github.com>
2025-01-10 04:45:55 +0000

    use tac instead of cat

--- Dispatch/cleanup.sh
+++ Dispatch/cleanup.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/bash
 
-thisscript=$(realpath --canonicalize-existing "${0}")
-thispath="${thisscript%/*}/"
-cd "${thispath}"
-
-filelist='artifactlist.txt'
-
 curl --location \
   --header 'Accept: application/vnd.github+json' \
   --header "Authorization: Bearer ${GITHUB_TOKEN}" \
   --header 'X-GitHub-Api-Version: 2022-11-28' \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts" |
   jq '[.artifacts[] | [.name,.id]]' |
-  jq --raw-output '.[] | @tsv' > "${filelist}"
-
-cat "${filelist}" |
+  jq --raw-output '.[] | @tsv'
   sed --silent '/^segment-[0-9]\+\t/p' |
   sed 's/\t/ /' |
+  tac |
   while read -r line
   do
     filename="${line%% *}"
